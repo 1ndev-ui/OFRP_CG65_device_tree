cabe7468070569f91d47fe70ca7c2d575789d4a3
1ndev-ui <1ndev-ui@github>
2024-02-28 01:32:36 -0800

    Removed GSM

--- twrp_CG65.mk
+++ twrp_CG65.mk
@@ -15,7 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/vortex/CG65/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
