7cc5d0d4cbc2a30387e6df34a943c3e670a52144
1ndev-ui <1ndev-ui@github>
2024-03-01 10:40:26 -0800

    Prep bootimage_partition_size change; Remove drastic reductions

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,42 @@
-export TARGET_ARCH="arm64-v8a"
 
-# Some about us
+# General Configuration
+export ALLOW_MISSING_DEPENDENCIES=true
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export LC_ALL="C"
+export OF_MAINTAINER="1ndevelopment"
+export FOX_BUILD_TYPE="1ndev"
 export FOX_VERSION="R12.1"
-export OF_MAINTAINER=1ndevelopment
+export FOX_TARGET_DEVICES="CG65"
+export FOX_BUILD_DEVICE="CG65"
+export TARGET_ARCH="arm64-v8a"
+export OF_CLASSIC_LEDS_FUNCTION=0
+export FOX_DELETE_AROMAFM=0
+export OF_CLOCK_POS=1
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+export OF_FORCE_PREBUILT_KERNEL=1
+export OF_NO_SPLASH_CHANGE=0
+export OF_STATUS_INDENT_RIGHT=48
+export OF_STATUS_INDENT_LEFT=48
+export OF_WIPE_METADATA_AFTER_DATAFORMAT=1
+export OF_OPTIONS_LIST_NUM=8
+
+# Partition Configurations
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=0
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system_a"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor_a"
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+
+# Security Configurations
+export OF_ADVANCED_SECURITY=1
+export OF_KEEP_DM_VERITY=0
+export OF_DISABLE_FORCED_ENCRYPTION=1
+export OF_FORCE_DISABLE_FORCED_ENCRYPTION=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export OF_USE_SYSTEM_FINGERPRINT=1
 
 # Build environment stuffs
-export FOX_BUILD_DEVICE="CG65"
-export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export TARGET_DEVICE_ALT="Vortex, CG65"
 export BUILD_USERNAME="nobody"
@@ -15,12 +45,6 @@ export BUILD_HOSTNAME="android-build"
 # Use magisk boot for patching
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-
-# We have a/b partitions
-export FOX_AB_DEVICE=1
-export FOX_VIRTUAL_AB_DEVICE=1
-export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=0
 
 # Screen specifications
 export OF_STATUS_INDENT_LEFT=48
@@ -31,19 +55,15 @@ export OF_SCREEN_H=1600
 export OF_STATUS_H=120
 
 # Device stuff
-export OF_KEEP_FORCED_ENCRYPTION=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 export OF_FBE_METADATA_MOUNT_IGNORE=1
 export OF_PATCH_AVB20=1
 export OF_USE_LEGACY_BATTERY_SERVICES=1
-export FOX_DRASTIC_SIZE_REDUCTION="1"
-export OF_FORCE_PREBUILT_KERNEL=1
 
 # Run a process after formatting data to work-around MTP issues
 export OF_RUN_POST_FORMAT_PROCESS=1
 
 # OTA for custom ROMs
-export OF_KEEP_DM_VERITY=1
 export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
@@ -61,9 +81,6 @@ export OF_DEFAULT_KEYMASTER_VERSION=4.1
 # Magisk
 export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
 
-# Dont install AROMAFM
-export FOX_DELETE_AROMAFM=1
-
 # Add some extras
 export FOX_USE_ZIP_BINARY=1
 export FOX_USE_TAR_BINARY=1
@@ -75,3 +92,9 @@ export FOX_USE_NANO_EDITOR=1
 export OF_DONT_KEEP_LOG_HISTORY=1
 export OF_NO_SPLASH_CHANGE=1
 export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
+
+# Applications Configurations
+export FOX_ENABLE_APP_MANAGER=1
+
+# Custom Binaries to SD Card Configuration
+export FOX_CUSTOM_BINS_TO_SDCARD=1
